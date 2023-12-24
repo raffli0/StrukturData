3a384f86ff3eb6e0f4a8659a625d7da1867f0cf0
raffli0 <141337926+raffli0@users.noreply.github.com>
2023-12-24 13:50:12 +0700

    Update BubbleSort.java

--- BubbleSort/BubbleSort.java
+++ BubbleSort/BubbleSort.java
@@ -8,7 +8,7 @@ public class BubbleSort {
                 if(arr[j] > arr[j+1]) {
                     int temp = arr[j];
                     arr[j] = arr[j+1];
-                    arr[j] = temp;
+                    arr[j+1] = temp;
                 }
     }
 
