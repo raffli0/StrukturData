802cf034cb2736493eece7bb345811a1692a1379
raffli0 <141337926+raffli0@users.noreply.github.com>
2023-12-24 12:57:41 +0700

    Update BubbleSort.java

--- BubbleSort/BubbleSort.java
+++ BubbleSort/BubbleSort.java
@@ -1,54 +1,37 @@
-import java.util.Arrays;
+public class BubbleSort {
 
-class BubbleSort {
+    //fungsi untuk memproses bubblesort
     void bubbleSort(int arr[]) {
         int n = arr.length;
-
-        for (int i = 0; i < n - 1; i++) {
-
-            for (int j = 0; j < n - i - 1; j++) {
-
-                if (arr[j] > arr[j + 1]) {
-
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - 1; j++) 
+                if(arr[j] > arr[j+1]) {
                     int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                    arr[j] = arr[j+1];
+                    arr[j] = temp;
                 }
-            }
-        }
     }
 
+    // fungsi untuk memprint array
     void printArray(int arr[]) {
-        int n = arr.length;
-
-        System.out.print("[");
-        for(int i = 0; i < n; i++) {
-
-            if(i < n -1) {
-                System.out.print(arr[i] + ", ");
-            }else {
-                System.out.print(arr[i]);
-            }
-        }
-        System.out.println("]");
+        int n = arr.length; //panjang array = 7
+        for (int i = 0; i < n; i++) 
+            System.out.println(arr[i] + " ");
+        System.out.println();
     }
-    
-public static void main(String[] args) {      
 
-        System.out.println("");
-        System.out.println("BUBBLE SORT\n");
-
+    //output
+    public static void main(String[] args) {
         BubbleSort obj = new BubbleSort();
         int arr[] = {54, 24, 35, 22, 10, 15, 80};
 
-        System.out.println("Array Awal ");
-        System.out.println(Arrays.toString(arr));
-        System.out.print("");
-
-        obj.bubbleSort(arr);
-        System.out.println("Array Urut ");
+        System.out.println("Array Sebelum Diurutkan");
         obj.printArray(arr);
-        
-    }
 
+        //calling fungsi bubblesort
+        obj.bubbleSort(arr);
+
+        System.out.println("Array Sesudah Diurutkan");
+        obj.printArray(arr);
+    }
 }
